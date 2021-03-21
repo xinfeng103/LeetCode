b1b5cfb55d215bf5e3219f9fea3e7090902efb5d
lixinfeng <xinfeng.love@gmail.com>
2021-03-21 21:27:27 +0800

    125. Valid Palindrome

---
+++ src/array/_125/Solution.java
@@ -0,0 +1,54 @@
+package array._125;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        char[] str2 = s.toLowerCase().toCharArray(); //转换成小写形式，再转化为字符数组
+        while (left < right) {
+            //判断是否为字母或数字，如果不是直接跳过
+            while (left < right && !Character.isLetterOrDigit(str2[left])) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(str2[right])) {
+                --right;
+            }
+
+            if (str2[left] != str2[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome1(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                ++left;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                --right;
+            }
+            if (left < right) {
+                if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                    return false;
+                }
+                ++left;
+                --right;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        Solution solution = new Solution();
+        System.out.println("是否为回文串" + solution.isPalindrome(s));
+    }
+}
---
+++ src/array/_125/validPalindrome.md
@@ -0,0 +1,11 @@
+# 125. Valid Palindrome
+## 题目
+- Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+>Input: s = "A man, a plan, a canal: Panama"
+>Output: true
+
+>Input: s = "race a car"
+>Output: false
+## 解题思路
+- 双指针
