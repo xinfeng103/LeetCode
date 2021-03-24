c75c4bd5e5644b41622f9db85cca07e6466f32e7
lixinfeng <xinfeng.love@gmail.com>
2021-03-24 21:00:36 +0800

    122. Best Time to Buy and Sell Stock II

---
+++ src/array/_122/Solution.java
@@ -0,0 +1,45 @@
+package array._122;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        //动态规划1
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    //优化的动态规划
+    public int maxProfit1(int[] prices) {
+        int n = prices.length;
+        int dp0 = 0, dp1 = -prices[0];
+        for (int i = 1; i < n; ++i) {
+            int newDp0 = Math.max(dp0, dp1 + prices[i]);
+            int newDp1 = Math.max(dp1, dp0 - prices[i]);
+            dp0 = newDp0;
+            dp1 = newDp1;
+        }
+        return dp0;
+    }
+
+    public int maxProfit2(int[] prices) {
+        int ans = 0;
+        int n = prices.length;
+        for (int i = 1; i < n; ++i) {
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 6, 5, 4, 2, 1};
+        Solution solution = new Solution();
+        int ans = solution.maxProfit(prices);
+        System.out.println(ans);
+    }
+}
