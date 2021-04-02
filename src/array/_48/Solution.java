b4760f96d40fb3cb0c06c9670ca60b03c86b49a7
lixinfeng <xinfeng.me@foxmail.com>
2021-04-02 21:32:14 +0800

    48. Rotate Image

--- src/array/_48/Solution.java
+++ src/array/_48/Solution.java
@@ -23,22 +23,8 @@ class Solution {
         }
     }
 
-    //原地旋转
-    public void rotate2(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n / 2; ++i) {
-            for (int j = 0; j < (n + 1) / 2; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-                matrix[j][n - i - 1] = temp;
-            }
-        }
-    }
-
     //用翻转代替旋转
-    public void rotate3(int[][] matrix) {
+    public void rotate2(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < matrix.length / 2; i++) {
             int[] tmp = matrix[i];
