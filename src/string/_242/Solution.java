b3db9915e9489a005d2381b3d6dd6e9119b7edf9
lixinfeng <xinfeng.me@foxmail.com>
2021-04-04 21:02:31 +0800

    242. Valid Anagram

---
+++ src/string/_242/Solution.java
@@ -0,0 +1,27 @@
+package string._242;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] arr = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "rat", t = "cat";
+        System.out.println(solution.isAnagram(s, t));
+    }
+}
---
+++ src/string/_242/ValidAnagram.md
@@ -0,0 +1,15 @@
+# 242. Valid Anagram
+
+## 题目
+
+- Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+> Input: s = "anagram", t = "nagaram"
+> Output: true
+
+> Input: s = "rat", t = "car"
+> Output: false
+
+## 解题思路
+
+- 哈希表
