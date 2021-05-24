66cbeb967b00f38dc7382ca005a8b541922791ce
lixinfeng <xinfeng.me@foxmail.com>
2021-05-24 19:59:43 +0800

    10. 裴波那契数列

--- src/Sword_Finger_Offer/CQueue09.java
+++ src/Sword_Finger_Offer/CQueue09.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Stack;
 
 /*
-todo 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+todo 9. 用两个栈实现队列：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
  分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，delete操作返回 -1 )
  */
 class CQueue09 {
---
+++ src/Sword_Finger_Offer/Fibonacci_10.java
@@ -0,0 +1,35 @@
+package Sword_Finger_Offer;
+
+// TODO 裴波那契数列,(n>0)
+public class Fibonacci_10 {
+    public long fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = 1, b = 1;
+        for (; n > 2; n--) {
+            b = b + a;
+            a = b - a;
+        }
+        return b;
+    }
+
+    public long fibonacci1(int n) {
+        int a = 0, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Fibonacci_10 solution = new Fibonacci_10();
+        for (int i = 0; i < 50; i++) {
+            System.out.println(solution.fibonacci(i));
+            System.out.println(solution.fibonacci1(i));
+            System.out.println();
+        }
+    }
+}
