5428fbdd20dd97d006549b9aa13671cdb05a5185
lixinfeng <xinfeng.me@foxmail.com>
2021-05-30 18:38:39 +0800

    剑指offer 14. 剪绳子

--- src/leetcode/editor/cn/JianShengZi_2.java
+++ src/leetcode/editor/cn/JianShengZi_2.java
@@ -29,7 +29,11 @@ public class JianShengZi_2 {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int cuttingRope(int n) {
-        return 0;
+        if(n <= 3) return n - 1;
+        int a = n / 3, b = n % 3;
+        if(b == 0) return (int)Math.pow(3, a);
+        if(b == 1) return (int)Math.pow(3, a - 1) * 4;
+        return (int)Math.pow(3, a) * 2;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
