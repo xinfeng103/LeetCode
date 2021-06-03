a25a152efb83f5b3db8f1ec2f87ae9d708b2916f
lixinfeng <xinfeng.me@foxmail.com>
2021-06-03 19:33:00 +0800

    143. 重排链表

---
+++ src/leetcode/editor/cn/ReorderList.java
@@ -0,0 +1,100 @@
+
+//给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+//将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→… 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 示例 1: 
+//
+// 给定链表 1->2->3->4, 重新排列为 1->4->2->3. 
+//
+// 示例 2: 
+//
+// 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3. 
+
+
+package leetcode.editor.cn;
+
+public class ReorderList {
+    public static void main(String[] args) {
+//        ListNode head = new ListNode(1,new ListNode(2,new ListNode(3,new ListNode(4,new ListNode(5,null)))));
+        ListNode head = new ListNode(1, null);
+        Solution solution = new ReorderList().new Solution();
+        solution.reorderList(head);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    //  Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public void reorderList(ListNode head) {
+            if (head == null) {
+                return;
+            }
+            ListNode mid = middleNode(head);
+            ListNode l1 = head;
+            ListNode l2 = mid.next;
+            mid.next = null;
+            l2 = reverseList(l2);
+            mergeList(l1, l2);
+        }
+
+        // 利用快慢指针找链表的中间节点
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+
+        // 反转链表，遍历当前位置（保存前后位置）
+        public ListNode reverseList(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+
+        // 合并链表
+        public void mergeList(ListNode l1, ListNode l2) {
+            ListNode l1_tmp;
+            ListNode l2_tmp;
+            while (l1 != null && l2 != null) {
+                l1_tmp = l1.next;
+                l1.next = l2;
+                l1 = l1_tmp;
+
+                l2_tmp = l2.next;
+                l2.next = l1;
+                l2 = l2_tmp;
+            }
+        }
+    }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
