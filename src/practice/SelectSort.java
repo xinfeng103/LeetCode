b442ad90c5aa4ea0eeb4bd56caa0a63e592681ee
lixinfeng <xinfeng.me@foxmail.com>
2021-06-20 10:53:59 +0800

    练习：选择排序

--- src/practice/SelectSort.java
+++ src/practice/SelectSort.java
@@ -1,4 +1,27 @@
 package practice;
 
+import java.util.Arrays;
+
 public class SelectSort {
+    public static void selectSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) min = j;
+            }
+            if (min != i) {
+                int tmp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,2,3,4,5};
+        System.out.println("排序前" + Arrays.toString(arr));
+        selectSort(arr);
+        System.out.println("排序后" + Arrays.toString(arr));
+    }
 }
