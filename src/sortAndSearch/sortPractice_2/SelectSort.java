18bd3b502b8942bf17423339c9f4604b482d4095
lixinfeng <xinfeng.me@foxmail.com>
2021-07-05 17:20:26 +0800

    选择排序

---
+++ src/sortAndSearch/sortPractice_2/SelectSort.java
@@ -0,0 +1,29 @@
+package sortAndSearch.sortPractice_2;
+
+import java.util.Arrays;
+
+public class SelectSort {
+    public static void selectSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                int tmp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{-1, 3, 4, 2, 5};
+        System.out.println("排序前" + Arrays.toString(arr));
+        selectSort(arr);
+        System.out.println("排序后" + Arrays.toString(arr));
+    }
+}
