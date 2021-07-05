e8aea783e82f70917be3eda7d786bda595e1bf38
lixinfeng <xinfeng.me@foxmail.com>
2021-07-05 17:15:18 +0800

    插入排序

---
+++ src/sortAndSearch/sortPractice_2/InsertSort.java
@@ -0,0 +1,26 @@
+package sortAndSearch.sortPractice_2;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void insertSort(int[] arr) {
+        int n = arr.length;
+        int i, j, cur;
+        for (i = 1; i < n; i++) {
+            cur = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > cur) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = cur;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3, 2, 1};
+        System.out.println("排序前" + Arrays.toString(arr));
+        insertSort(arr);
+        System.out.println("排序后" + Arrays.toString(arr));
+    }
+}
