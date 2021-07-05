e8141765c631ccc26c6864288a02aff0edd5e88c
lixinfeng <xinfeng.me@foxmail.com>
2021-07-05 17:06:10 +0800

    冒泡排序

---
+++ src/sortAndSearch/sortPractice_2/BubbleSort.java
@@ -0,0 +1,30 @@
+package sortAndSearch.sortPractice_2;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n - 1; i > 0; i--) {
+            boolean isExchange = false;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    isExchange = true;
+                }
+            }
+            if (!isExchange) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,2,1};
+        System.out.println("排序前" + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("排序后" + Arrays.toString(arr));
+    }
+}
