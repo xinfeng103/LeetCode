e932ffe389421aac06e6e1b0d2fd9e036fe6bc56
lixinfeng <xinfeng.me@foxmail.com>
2021-07-06 22:23:29 +0800

    小练习

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        int[] nums = new int[10];
+        System.out.println(nums.length);
+        System.out.println(Arrays.toString(numsaz));
+    }
+}
