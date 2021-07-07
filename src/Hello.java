078b25d042fd44dd58e931053b523902c1c55cec
lixinfeng <xinfeng.me@foxmail.com>
2021-07-07 20:07:12 +0800

    94. 二叉树的中序遍历

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         int[] nums = new int[10];
         System.out.println(nums.length);
-        System.out.println(Arrays.toString(numsaz));
+        System.out.println(Arrays.toString(nums));
     }
 }
---
+++ src/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -0,0 +1,77 @@
+//给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+// 示例 1：
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+// 示例 2：
+//输入：root = []
+//输出：[]
+// 示例 3：
+//输入：root = [1]
+//输出：[1]
+// 示例 4：
+//输入：root = [1,2]
+//输出：[2,1]
+// 示例 5：
+//输入：root = [1,null,2]
+//输出：[1,2]
+// 提示：
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100
+
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal{
+    public static void main(String[] args){
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+ // Definition for a binary tree node.
+//  public class TreeNode {
+//      int val;
+//      TreeNode left;
+//      TreeNode right;
+//      TreeNode() {}
+//      TreeNode(int val) { this.val = val; }
+//      TreeNode(int val, TreeNode left, TreeNode right) {
+//          this.val = val;
+//          this.left = left;
+//          this.right = right;
+//      }
+//  }
+
+class Solution {
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+    public void inorder(TreeNode root,List<Integer> res){
+        if(root==null) return;
+        inorder(root.left,res);
+        res.add(root.val);
+        inorder(root.right,res);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stk = new Stack<>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
