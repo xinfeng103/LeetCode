3a56e017c1444a9b42febca8c75df348e181ed93
lixinfeng02 <lixinfeng02@58.com>
2021-08-19 17:15:26 +0800

    牛客网-网易21杭研真题-小易的考试成绩

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Hello {
+    public void test() {
+        Hashtable<String, String> ht = new Hashtable<>();
+
+        ConcurrentHashMap chm = new ConcurrentHashMap();
+
+        Integer i = Integer.valueOf(1);
+
+        ReentrantLock lock = new ReentrantLock();
+
+        HashMap<String, String> hm = new HashMap<>();
+
+
+        Thread th = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
 }
+
