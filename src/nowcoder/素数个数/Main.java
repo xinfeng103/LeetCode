9fd39deecfb7256041907065f4af98fe8e8ab306
lixinfeng02 <xinfeng.me@foxmail.com>
2021-08-19 17:41:44 +0800

    牛客网-网易21杭研真题-素数个数

---
+++ src/nowcoder/素数个数/Main.java
@@ -0,0 +1,15 @@
+package nowcoder.素数个数;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += sc.nextInt() >> 1;
+        }
+        System.out.println(ans);
+    }
+}
